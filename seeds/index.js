6c154da800e93542cc010a7fc7efddf4bd471b4a
llstylefish <haozhen.shen@mail.utoronto.ca>
2023-05-23 21:32:37 -0400

    added contrroller

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '646c08cce5020fcb518c99ea',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
