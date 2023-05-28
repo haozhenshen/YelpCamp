613e44ae93d0c1f235355588d9fada5f03df35d4
llstylefish <haozhen.shen@mail.utoronto.ca>
2023-05-28 13:56:02 -0400

    fix logout error

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,14 @@ router.route('/login')
     .get(users.renderLogin)
     .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-router.get('/logout', users.logout)
+    router.get('/logout', (req, res, next) => {
+        req.logout(function (err) {
+            if (err) {
+                return next(err);
+            }
+            req.flash('success', 'Goodbye!');
+            res.redirect('/campgrounds');
+        });
+    }); 
 
 module.exports = router;
